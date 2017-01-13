1a0cdb07366741e79e35ac025d3ea35f319f76a9
Keith Howling <kehowli@microsoft.com>
2017-01-13 10:30:50 +0000

    env setup

--- env_setup/azure_vmss_swarm.sh
+++ env_setup/azure_vmss_swarm.sh
@@ -26,48 +26,47 @@ function gen_docker_certs {
   DAYS=1460
   PASS=$(openssl rand -hex 16)
 
-  mkdir -p ${DOCKER_CERT_PATH}
+  mkdir -p $DOCKER_CERT_PATH
   current_dir=$PWD
-  cd ${DOCKER_CERT_PATH}
+  cd $DOCKER_CERT_PATH
   # remove certificates from previous execution.
   rm -f *.pem *.srl *.csr *.cnf
 
 
   # generate CA private and public keys
   echo 01 > ca.srl
-  openssl genrsa -des3 -out ca-key.pem -passout pass:$PASS 2048
-  openssl req -subj "/CN=${HOSTNAME}/" -new -x509 -days $DAYS -passin pass:$PASS -key ca-key.pem -out ca.pem
+  openssl genrsa -des3 -out ca-key.pem -passout pass:$PASS 2048 2>/dev/null
+  openssl req -subj "/CN=${HOSTNAME}/" -new -x509 -days $DAYS -passin pass:$PASS -key ca-key.pem -out ca.pem 2>/dev/null
 
   # create a server key and certificate signing request (CSR)
-  openssl genrsa -des3 -out server-key.pem -passout pass:$PASS 2048
-  openssl req -new -key server-key.pem -out server.csr -passin pass:$PASS -subj "/CN=${HOSTNAME}/"
+  openssl genrsa -des3 -out server-key.pem -passout pass:$PASS 2048 2>/dev/null
+  openssl req -new -key server-key.pem -out server.csr -passin pass:$PASS -subj "/CN=${HOSTNAME}/" 2>/dev/null
 
   # sign the server key with our CA
-  openssl x509 -req -days $DAYS -passin pass:$PASS -in server.csr -CA ca.pem -CAkey ca-key.pem -out server-cert.pem
+  openssl x509 -req -days $DAYS -passin pass:$PASS -in server.csr -CA ca.pem -CAkey ca-key.pem -out server-cert.pem 2>/dev/null
 
   # create a client key and certificate signing request (CSR)
-  openssl genrsa -des3 -out key.pem -passout pass:$PASS 2048
-  openssl req -subj '/CN=client' -new -key key.pem -out client.csr -passin pass:$PASS
+  openssl genrsa -des3 -out key.pem -passout pass:$PASS 2048 2>/dev/null
+  openssl req -subj '/CN=client' -new -key key.pem -out client.csr -passin pass:$PASS 2>/dev/null
 
   # create an extensions config file and sign
   echo extendedKeyUsage = clientAuth > extfile.cnf
-  openssl x509 -req -days $DAYS -passin pass:$PASS -in client.csr -CA ca.pem -CAkey ca-key.pem -out cert.pem -extfile extfile.cnf
+  openssl x509 -req -days $DAYS -passin pass:$PASS -in client.csr -CA ca.pem -CAkey ca-key.pem -out cert.pem -extfile extfile.cnf 2>/dev/null
 
   # remove the passphrase from the client and server key
-  openssl rsa -in server-key.pem -out server-key.pem -passin pass:$PASS
-  openssl rsa -in key.pem -out key.pem -passin pass:$PASS
+  openssl rsa -in server-key.pem -out server-key.pem -passin pass:$PASS 2>/dev/null
+  openssl rsa -in key.pem -out key.pem -passin pass:$PASS 2>/dev/null
 
   # remove generated files that are no longer required
   rm -f ca-key.pem ca.srl client.csr extfile.cnf server.csr
   cd $current_dir
 }
 
+echo "creating docker engine tls certs (${DOCKER_CERT_PATH})...."
 gen_docker_certs
 
-PARAMS="{\"swarm_name\": { \"value\": \"${SWARNNAME}\"}, \"swarm_nodes\": { \"value\": \"${NODES}\"}, \"adminUsername\": { \"value\": \"$(whoami)\"}, \"ssh_keyData\": { \"value\": \"`cat  ~/.ssh/id_rsa.pub`\" }, \"vnet_resource_group\": { \"value\": \"${RGNAME}\"}, \"vnet_name\": { \"value\": \"${VNET}\"}, \"vnet_subnet_name\": { \"value\": \"${SUB}\"}, \"docker_ca_base64\": { \"value\": \"`base64 -w 0 $DOCKER_CERT_PATH/ca.pem`\"}, \"docker_key_base64\": { \"value\": \"`base64 -w 0 $DOCKER_CERT_PATH/server-key.pem`\"}, \"docker_cert_base64\": { \"value\": \"`base64 -w 0 $DOCKER_CERT_PATH/server-cert.pem`\"}}"
 
-#echo $PARAMS
-read -p "Create the Resource Group \"${NEWRG}\" (y/n): " answer
+read -p "create the Resource Group \"${NEWRG}\" (y/n): " answer
 case ${answer:0:1} in
     y|Y )
         echo "creating group....."
@@ -75,15 +74,27 @@ case ${answer:0:1} in
     ;;
 esac
 
+
 echo "creating swarm & loadbalancer....."
+PARAMS="{\"swarm_name\": { \"value\": \"${SWARNNAME}\"}, \"swarm_nodes\": { \"value\": \"${NODES}\"}, \"adminUsername\": { \"value\": \"$(whoami)\"}, \"ssh_keyData\": { \"value\": \"$(cat  ~/.ssh/id_rsa.pub)\" }, \"vnet_resource_group\": { \"value\": \"${RGNAME}\"}, \"vnet_name\": { \"value\": \"${VNET}\"}, \"vnet_subnet_name\": { \"value\": \"${SUB}\"}, \"docker_ca_base64\": { \"value\": \"$(base64 -w 0 $DOCKER_CERT_PATH/ca.pem)\"}, \"docker_key_base64\": { \"value\": \"$(base64 -w 0 $DOCKER_CERT_PATH/server-key.pem)\"}, \"docker_cert_base64\": { \"value\": \"$(base64 -w 0 $DOCKER_CERT_PATH/server-cert.pem)\"}}"
 az group deployment create -g  $NEWRG -n ${NEWRG}_d1 --template-file ./azure_vmss_swarm.json --parameters "${PARAMS}"
 
-echo "creating swarm (manager ${HOSTNAME})....."
-sleep 5
-ssh ${HOSTNAME} -oStrictHostKeyChecking=no -p 50000 'docker swarm init'
+if [ $? -ne 0 ]; then
+    echo "Failed to create Cluster"
+    exit 1
+fi
 
-read -p "Copy and Paste join command from above: " joinans
-ssh ${HOSTNAME} -oStrictHostKeyChecking=no -p 50001 "${joinans}"
+
+echo "initialising swarm (manager ${HOSTNAME})....."
+sleep 5
+init_output=$(ssh ${HOSTNAME} -oStrictHostKeyChecking=no -p 50000 'docker swarm init')
+join_cmd=$(echo $init_output | grep -oP '(?<=command: ).+?(?=To)')
+
+for i in $(seq 2 $NODES); do  
+  echo "adding node ${i} to swarm...."
+  ssh ${HOSTNAME} -oStrictHostKeyChecking=no -p $((50000+i-1)) "${join_cmd//\\/}";
+done
+
 
 echo "To point docker cli to the engine, run: 
 export DOCKER_TLS_VERIFY=1
@@ -91,5 +102,3 @@ export DOCKER_HOST=tcp://${HOSTNAME}:2376
 export DOCKER_CERT_PATH=${DOCKER_CERT_PATH}
 docker node list
 "
-# look at cluster
-# > docker --tls=true node list
